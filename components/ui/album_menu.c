8a07030bfc6ef3a8dc29583573845c70a1509487
ekim <ekim.tecul@yahoo.fr>
2020-04-18 22:23:51 +0200

    add album menu

--- components/ui/album_menu.c
+++ components/ui/album_menu.c
@@ -1,4 +1,4 @@
-#include "artist_menu.h"
+#include "album_menu.h"
 
 #include <stdio.h>
 #include <assert.h>
@@ -7,13 +7,12 @@
 #include "esp_log.h"
 
 #include "db.h"
-#include "album_menu.h"
 
 #define container_of(ptr, type, member) ({ \
 	const typeof( ((type *)0)->member ) *__mptr = (ptr); \
 	(type *)( (char *)__mptr - offsetof(type,member) );})
 
-static const char* TAG = "rv3.artist_menu";
+static const char* TAG = "rv3.album_menu";
 
 enum menu_button {
 	MENU_MENU,
@@ -26,7 +25,7 @@ enum menu_button {
 	MENU_BTN_NB
 };
 
-struct artist_menu {
+struct album_menu {
 	struct ui_cbs cbs;
 	lv_obj_t *prev_scr;
 	lv_obj_t *scr;
@@ -35,40 +34,40 @@ struct artist_menu {
 	int page_nb;
 	int page_count;
 	void *db_hdl;
-	int artist_nb;
+	int album_nb;
+	char *artist;
 };
 
-static inline struct artist_menu *get_artist_menu()
+static inline struct album_menu *get_album_menu()
 {
 	struct ui_cbs *cbs = lv_obj_get_user_data(lv_disp_get_scr_act(NULL));
-	return container_of(cbs, struct artist_menu, cbs);
+	return container_of(cbs, struct album_menu, cbs);
 }
 
-static void artist_menu_destroy(struct artist_menu *menu)
+static void album_menu_destroy(struct album_menu *menu)
 {
-	db_close(menu->db_hdl);
 	lv_obj_del(menu->scr);
 	free(menu);
 }
 
-static void handle_back_event(struct artist_menu *menu)
+static void handle_back_event(struct album_menu *menu)
 {
 	lv_disp_load_scr(menu->prev_scr);
-	artist_menu_destroy(menu);
+	album_menu_destroy(menu);
 }
 
 static void destroy_chained(struct ui_cbs *cbs)
 {
-	struct artist_menu *menu = container_of(cbs, struct artist_menu, cbs);
+	struct album_menu *menu = container_of(cbs, struct album_menu, cbs);
 	ui_hdl prev;
 
 	prev = lv_obj_get_user_data(menu->prev_scr);
 	prev->destroy_chained(prev);
 
-	artist_menu_destroy(menu);
+	album_menu_destroy(menu);
 }
 
-static enum menu_button get_btn_id_from_obj(struct artist_menu *menu, lv_obj_t *btn)
+static enum menu_button get_btn_id_from_obj(struct album_menu *menu, lv_obj_t *btn)
 {
 	int i;
 
@@ -80,10 +79,10 @@ static enum menu_button get_btn_id_from_obj(struct artist_menu *menu, lv_obj_t *
 	return MENU_BTN_NB;
 }
 
-static void artist_menu_setup_page(struct artist_menu *menu, int page_nb)
+static void album_menu_setup_page(struct album_menu *menu, int page_nb)
 {
 	int page_start = page_nb * 3;
-	int page_end = page_start + 3 < menu->artist_nb ? page_start + 3 : menu->artist_nb;
+	int page_end = page_start + 3 < menu->album_nb ? page_start + 3 : menu->album_nb;
 
 	enum menu_button btn_id = MENU_BTN_0;
 	int i;
@@ -93,19 +92,19 @@ static void artist_menu_setup_page(struct artist_menu *menu, int page_nb)
 	lv_obj_set_hidden(menu->btn[MENU_DOWN], page_nb == menu->page_count - 1);
 
 	for (i = page_start; i < page_end; i++, btn_id++) {
-		char *artist_name = db_artist_get(menu->db_hdl, i);
+		char *album_name = db_album_get(menu->db_hdl, menu->artist, i);
 
 		lv_obj_set_hidden(menu->btn[btn_id], false);
-		lv_label_set_text(menu->label[btn_id], artist_name);
-		db_put_item(menu->db_hdl, artist_name);
+		lv_label_set_text(menu->label[btn_id], album_name);
+		db_put_item(menu->db_hdl, album_name);
 	}
 	for (i = page_end; i < page_start + 3; i++, btn_id++)
 		lv_obj_set_hidden(menu->btn[btn_id], true);
 }
 
-static void artist_menu_event_cb(lv_obj_t *btn, lv_event_t event)
+static void album_menu_event_cb(lv_obj_t *btn, lv_event_t event)
 {
-	struct artist_menu *menu = get_artist_menu();
+	struct album_menu *menu = get_album_menu();
 	enum menu_button btn_id = get_btn_id_from_obj(menu, btn);
 
 	if (event != LV_EVENT_CLICKED)
@@ -119,23 +118,22 @@ static void artist_menu_event_cb(lv_obj_t *btn, lv_event_t event)
 		handle_back_event(menu);
 		break;
 	case MENU_UP:
-		artist_menu_setup_page(menu, menu->page_nb - 1);
+		album_menu_setup_page(menu, menu->page_nb - 1);
 		break;
 	case MENU_DOWN:
-		artist_menu_setup_page(menu, menu->page_nb + 1);
+		album_menu_setup_page(menu, menu->page_nb + 1);
 		break;
 	case MENU_BTN_0:
 	case MENU_BTN_1:
 	case MENU_BTN_2:
 		ESP_LOGI(TAG, "You select %s", lv_label_get_text(menu->label[btn_id]));
-		album_menu_create(menu->db_hdl, lv_label_get_text(menu->label[btn_id]));
 		break;
 	default:
 		assert(0);
 	}
 }
 
-static void setup_new_screen(struct artist_menu *menu)
+static void setup_new_screen(struct album_menu *menu)
 {
 	if (menu->scr)
 		lv_obj_del(menu->scr);
@@ -146,7 +144,7 @@ static void setup_new_screen(struct artist_menu *menu)
 	lv_obj_set_size(menu->scr, 320, 240);
 }
 
-static void setup_artist_menu(struct artist_menu *menu)
+static void setup_album_menu(struct album_menu *menu)
 {
 	const int sizes[MENU_BTN_NB][2] = {
 		{60, 55}, {60, 55}, {60, 55}, {60, 55},
@@ -168,7 +166,7 @@ static void setup_artist_menu(struct artist_menu *menu)
 		lv_obj_set_hidden(menu->btn[i], true);
 		menu->label[i] = lv_label_create(menu->btn[i], NULL);
 		assert(menu->label);
-		lv_obj_set_event_cb(menu->btn[i], artist_menu_event_cb);
+		lv_obj_set_event_cb(menu->btn[i], album_menu_event_cb);
 	}
 	lv_obj_set_hidden(menu->btn[MENU_MENU], false);
 	lv_label_set_text(menu->label[MENU_MENU], "MENU");
@@ -177,29 +175,26 @@ static void setup_artist_menu(struct artist_menu *menu)
 	lv_label_set_text(menu->label[MENU_UP], "UP");
 	lv_label_set_text(menu->label[MENU_DOWN], "DOWN");
 
-	artist_menu_setup_page(menu, 0);
+	album_menu_setup_page(menu, 0);
 }
 
-ui_hdl artist_menu_create()
+ui_hdl album_menu_create(void *db_hdl, char *artist)
 {
-	struct artist_menu *menu;
+	struct album_menu *menu;
 
 	menu = malloc(sizeof(*menu));
 	if (!menu)
 		return NULL;
 	memset(menu, 0, sizeof(*menu));
 
-	menu->db_hdl = db_open("/sdcard/music.db");
-	if (!menu->db_hdl) {
-		free(menu);
-		return NULL;
-	}
-	menu->artist_nb = db_artist_get_nb(menu->db_hdl);
-	menu->page_count = ((menu->artist_nb  - 1) / 3) + 1;
+	menu->db_hdl = db_hdl;
+	menu->artist = artist;
+	menu->album_nb = db_album_get_nb(menu->db_hdl, artist);
+	menu->page_count = ((menu->album_nb  - 1) / 3) + 1;
 
 	menu->prev_scr = lv_disp_get_scr_act(NULL);
 	menu->cbs.destroy_chained = destroy_chained;
-	setup_artist_menu(menu);
+	setup_album_menu(menu);
 
 	return &menu->cbs;
 }
---
+++ components/ui/include/album_menu.h
@@ -0,0 +1,8 @@
+#ifndef __ALBUM_MENU__
+#define __ALBUM_MENU__ 1
+
+#include "ui.h"
+
+ui_hdl album_menu_create(void *db_hdl, char *artist);
+
+#endif
